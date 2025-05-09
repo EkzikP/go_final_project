e0fc8b82295d13ec230ce8bd247f1ba04a55c556
Boris Zhdanov <128117140+EkzikP@users.noreply.github.com>
2025-05-09 18:04:57 +0500

    Revert "13-go-basic"

--- tests/settings.go
+++ tests/settings.go
@@ -2,6 +2,6 @@ package tests
 
 var Port = 7540
 var DBFile = "../scheduler.db"
-var FullNextDate = true
-var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoYXNoIjoiYTFlYjdjZTFiMzEzMmQyNjlmMTVhY2Q5Yzk3OGM0ODFjOTM4OTUwNiJ9.z0Jyt1rBDYUdF4Igmzr-ENZJ0-WmOEM8hhx0Ou11KEA`
+var FullNextDate = false
+var Search = false
+var Token = ``
